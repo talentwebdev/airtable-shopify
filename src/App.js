61a78d791d316f36706fa536363e5b6e99e1109e
TalentWebDev <49565243+talentwebdev@users.noreply.github.com>
2021-04-13 04:58:35 +0800

    Add axios to the app and add logo dropdown.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,50 @@
 import React from 'react';
 import { base } from 'common/lib/airtable';
 
-function App() {
-    const [locations, setLocations] = React.useState([]);
-
-    const customerName = window.customerName;
-
-    React.useEffect(() => {
-        // fetch airtable data
-        base('Job Leads')
+const fetchData = () => {
+    // fetch airtable data
+    return new Promise((resolve) => {
+        base('ICO Logos')
             .select({
                 maxRecords: 3,
-                view: 'All Positions',
-                filterByFormula: `{Company}='${
-                    window.customerName || 'Tim De Vries'
-                }'`
+                view: 'Grid view',
+                filterByFormula: `{name}='${window.logo_tag}'`
             })
             .eachPage((records, fetchNextPage) => {
+                console.log(records);
                 // read location info for each record
 
-                const locations = records.map((record) =>
-                    record.get('Location')
-                );
+                const locations = records.map((record) => record.get('Notes'));
+                resolve(locations);
 
-                setLocations(locations);
+                // setLocations(locations);
 
                 // fetch next page
                 fetchNextPage();
             });
+    });
+};
+
+/*
+const newFunction = async () => {
+    await fetchData();
+}
+*/
+
+function App() {
+    //const [locations, setLocations] = React.useState([]);
+
+    // const customerName = window.customerName;
+
+    const loadData = async () => {
+        const data = await fetchData();
+        // setLocations(data);
+        document.getElementById('product-logos').innerHTML = data;
+    };
+    React.useEffect(() => {
+        loadData();
     }, []);
-    return (
-        <div className="App">
-            <h1>{customerName}'s Locations</h1>
-            {locations.map((location) => (
-                <h2 key={location}>{location}</h2>
-            ))}
-        </div>
-    );
+    return <div className="App"></div>;
 }
 
 export default App;
--- src/common/lib/airtable.js
+++ src/common/lib/airtable.js
@@ -2,4 +2,4 @@ import Airtable from 'airtable';
 
 export const base = new Airtable({
     apiKey: process.env.REACT_APP_AIRTABLE_API_KEY
-}).base('appceB80JsqMqRdeS');
+}).base('appV5WNw0HZSQFPJR');
